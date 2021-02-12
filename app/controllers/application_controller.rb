c90a481ae8f87b5ea9b568d43124250d8f3b303d
Eric <eric.m.lombardo@gmail.com>
2021-02-12 12:42:20 -0500

    Build CRUD routes using Active Record and Sinatra

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,44 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    params.delete("_method")
+    @article = Article.find(params[:id])
+    @article.update(params)
+    
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    Article.find(params[:id]).destroy
+
+    redirect "/articles" 
+  end
 end
